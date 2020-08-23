04e245ad186cafc0a382b44f5cb4121e86a68b96
asoler000 <68714690+asoler000@users.noreply.github.com>
2020-08-23 07:48:57 -0500

    Update script3.js
    updated js was not loading before

--- script3.js
+++ script3.js
@@ -1,4 +1,6 @@
+// The Password generator will provide a password with 8-50  characters based on criteria the user specifies.
 
+//Assignment Code + Event Listener to prompt questions when button pushed
 document.querySelector("#generate").addEventListener("click", writePassword);
 
 // Various Arrays 
@@ -16,68 +18,67 @@ var confirmLowerCase;
 
 // Prompt to confirm how many characters the user would like in their password
 function generatePassword() {
-      // Determine character options for user
-      var confirmLength = prompt("How many characters would you like your password to contain?");
-      var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
-      var confirmNumericCharacter = confirm("How about numbers?");    
-      var confirmLowerCase = confirm("Let's make it extra spicy and add some lowercase characters?");
-      var confirmUpperCase = confirm("And the final touch to make this password muy calinete; let's throw in uppercase characters?");
+  var confirmLength = (prompt("How many characters would you like your password to contain?"));
 
+  // Loop if answer is outside the parameters 
+  while(confirmLength <= 7 || confirmLength >= 51) {
+      alert("Password length must be between 8-50 characters Try again");
+      var confirmLength = (prompt("How many characters would you like your password to contain?"));
+      } 
+
+      // Repeat back how many charactes the user will have  
+      alert(`Your password will have ${confirmLength} characters`);
+
+    // Determine parameters of password 
+    var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
+    var confirmNumericCharacter = confirm("How about numbers?");    
+    var confirmLowerCase = confirm("Include some lower case characters as well?");
+    var confirmUpperCase = confirm("You can make this password 'muy caliente` by adding some special characters. Would you like to add some");
+      // Loop if answer is outside the parameters 
       while(confirmUpperCase === false && confirmLowerCase === false && confirmSpecialCharacter === false && confirmNumericCharacter === false) {
         alert("You must choose at least one parameter");
-        var confirmSpecialCharacter = confirm("Let's try this again. Click OK to confirm if you would like to include special characters");
+        var confirmSpecialCharacter = confirm("Click OK to confirm if you would like to include special characters");
         var confirmNumericCharacter = confirm("Click OK to confirm if you would like to include numeric characters");    
         var confirmLowerCase = confirm("Click OK to confirm if you would like to include lowercase characters");
         var confirmUpperCase = confirm("Click OK to confirm if you would like to include uppercase characters");   
-    
-     
-// Loop if user does not pick 8 or more characters but the max is 123
-while(confirmLength <= 7 || confirmLength >= 124) {
-    alert("Password length must be between 8-124 characters Try again");
-    var confirmLength = (prompt("How many characters would you like your password to contain?"));
     } 
 
-    // This let's the user know the conFirmation on how many charcters the password will be
-    alert(`Your password will have ${confirmLength} characters`);
-
-
-  } 
-
-  var passwordCharacters = []
+      // Assign an action to the password parameters 
+      var passwordCharacters = []
       
-  if (confirmSpecialCharacter) {
-    passwordCharacters = passwordCharacters.concat(specialChar)
-  }
-
-  if (confirmNumericCharacter) {
-    passwordCharacters = passwordCharacters.concat(number)
-  }
-    
-  if (confirmLowerCase) {
-    passwordCharacters = passwordCharacters.concat(alphaLower)
-  }
-
-  if (confirmUpperCase) {
-    passwordCharacters = passwordCharacters.concat(alphaUpper)
-  }
-
-
-
-    // Empty string to be filled based on for loop selecting random characters from the array
-    var randomPassword = ""
-    
-    for (var i = 0; i < confirmLength; i++) {
-      randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
-      console.log(randomPassword)
+    if (confirmSpecialCharacter) {
+      passwordCharacters = passwordCharacters.concat(specialChar)
     }
-    return randomPassword;
+
+    if (confirmNumericCharacter) {
+      passwordCharacters = passwordCharacters.concat(number)
+    }
+      
+    if (confirmLowerCase) {
+      passwordCharacters = passwordCharacters.concat(alphaLower)
+    }
+
+    if (confirmUpperCase) {
+      passwordCharacters = passwordCharacters.concat(alphaUpper)
+    }
+
+      console.log(passwordCharacters)
+
+      // Empty string to be filled based on for loop selecting random characters from the array
+      var randomPassword = ""
+      
+      for (var i = 0; i < confirmLength; i++) {
+        randomPassword = randomPassword + passwordCharacters[Math.floor(Math.random() * passwordCharacters.length)];
+        console.log(randomPassword)
+      }
+      return randomPassword;
 }
 
 // Write password to the #password input
 function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
-  
-    passwordText.value = password;
-  }
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
 
